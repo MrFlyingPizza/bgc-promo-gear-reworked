4bc7c694893f6365405ed2cb0042044eaefc29cc
MrFlyingPizza <65780477+MrFlyingPizza@users.noreply.github.com>
2022-03-17 00:27:20 -0700

    added headers and tags to identify image blobs and serve correct type

--- bgc-promo-gear-backend/src/main/java/com/example/bgcpromogearreworked/api/products/images/ProductImageBlobService.java
+++ bgc-promo-gear-backend/src/main/java/com/example/bgcpromogearreworked/api/products/images/ProductImageBlobService.java
@@ -5,12 +5,14 @@ import com.azure.storage.blob.BlobClient;
 import com.azure.storage.blob.BlobContainerClient;
 import com.azure.storage.blob.BlobServiceClient;
 import com.azure.storage.blob.BlobServiceClientBuilder;
+import com.azure.storage.blob.models.BlobHttpHeaders;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.UUID;
 
 @Service
@@ -35,7 +37,7 @@ public class ProductImageBlobService {
     }
 
     /**
-     * Saves an image as a blob to the blob container that has the given productId as the name.
+     * Saves an image as a blob to the blob container that has the given productId as the name along with product ID and random UUID as the tag
      * @param productId The ID of the product which the blob container is named by.
      * @param imageFile The image file to be saved as a blob to the blob container.
      * @return Returns {@link ImageBlobResult} if successful, otherwise return null.
@@ -51,6 +53,8 @@ public class ProductImageBlobService {
             UUID imageBlobId = UUID.randomUUID();
             BlobClient blobClient = blobContainerClient.getBlobClient(buildBlobName(imageBlobId));
             blobClient.upload(BinaryData.fromBytes(imageFile.getBytes()));
+            blobClient.setHttpHeaders(buildHeaders(imageFile.getContentType()));
+            blobClient.setTags(buildTags(productId, imageBlobId));
             return new ImageBlobResult(blobClient.getBlobUrl(), imageBlobId);
         } catch (IOException e) {
             e.printStackTrace();
@@ -63,6 +67,19 @@ public class ProductImageBlobService {
             throw new NullPointerException();
         }
         blobContainerClient.getBlobClient(buildBlobName(imageBlobId)).delete();
+    }
+
+    private HashMap<String, String> buildTags(Long productId, UUID blobId) {
+        final String PRODUCT_ID_TAG_KEY = "product_id";
+        final String BLOB_ID_TAG_KEY = "blob_id";
+        HashMap<String, String> tags = new HashMap<>();
+        tags.put(PRODUCT_ID_TAG_KEY, productId.toString());
+        tags.put(BLOB_ID_TAG_KEY, blobId.toString());
+        return tags;
+    }
+
+    private static BlobHttpHeaders buildHeaders(String contentType) {
+        return new BlobHttpHeaders().setContentType(contentType);
     }
 
     private static String buildBlobName(UUID blobId) {
