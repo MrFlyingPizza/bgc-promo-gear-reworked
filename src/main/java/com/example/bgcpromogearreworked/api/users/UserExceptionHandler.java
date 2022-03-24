2cfc42b71988427ae65e20286b607d1a41341690
MrFlyingPizza <65780477+MrFlyingPizza@users.noreply.github.com>
2022-03-24 00:08:23 -0700

    added exception handling for no authentication

--- src/main/java/com/example/bgcpromogearreworked/api/users/UserExceptionHandler.java
+++ src/main/java/com/example/bgcpromogearreworked/api/users/UserExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.example.bgcpromogearreworked.api.users;
 
 import com.example.bgcpromogearreworked.api.users.exceptions.UserNotFoundException;
+import com.example.bgcpromogearreworked.api.users.exceptions.UserNotAuthenticatedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -13,6 +14,12 @@ public class UserExceptionHandler {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     private String handleUserNotFound(UserNotFoundException exception) {
         return "User could not be found.";
+    }
+
+    @ExceptionHandler(UserNotAuthenticatedException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    private String handleUserNotAuthenticated(UserNotAuthenticatedException exception) {
+        return "Current user is not authenticated and cannot be identified to be used in requests.";
     }
 
 }
