f55a104146f7b33e8d3f5f7e3aa541489e1b6d48
Richard Gao <RGao@bgcengineering.ca>
2022-04-06 11:01:01 -0700

    made supplier map unlimited initialization

--- src/main/java/com/example/bgcpromogearreworked/api/orders/inventorymanagement/InventoryModifierSupplier.java
+++ src/main/java/com/example/bgcpromogearreworked/api/orders/inventorymanagement/InventoryModifierSupplier.java
@@ -9,49 +9,35 @@ import java.util.Map;
 class InventoryModifierSupplier {
 
     // TODO: 2022-04-06 implement supplier functions
-    private static final Map<StatusDeltaKey, InventoryModifier> callbacks = Map.of(
-            new StatusDeltaKey(Order.Status.WAIT_LIST, null),
-            (inventoryLevel, change) -> {
-                inventoryLevel.setNeededQuantity(inventoryLevel.getNeededQuantity() + change);
-                return inventoryLevel;
-            },
-            new StatusDeltaKey(Order.Status.SUBMITTED, Order.Status.WAIT_LIST),
-            (inventoryLevel, change) -> {
-                return inventoryLevel;
-            },
-            new StatusDeltaKey(Order.Status.PROCESSING, Order.Status.SUBMITTED),
-            (inventoryLevel, change) -> {
-                return inventoryLevel;
-            },
-            new StatusDeltaKey(Order.Status.COMPLETED, Order.Status.PROCESSING),
-            (inventoryLevel, change) -> {
-                return inventoryLevel;
-            },
-            new StatusDeltaKey(Order.Status.CANCELLED, Order.Status.WAIT_LIST),
-            (inventoryLevel, change) -> {
-                return inventoryLevel;
-            },
-            new StatusDeltaKey(Order.Status.CANCELLED, Order.Status.SUBMITTED),
-            (inventoryLevel, change) -> {
-                return inventoryLevel;
-            },
-            new StatusDeltaKey(Order.Status.CANCELLED, Order.Status.PROCESSING),
-            (inventoryLevel, change) -> {
-                return inventoryLevel;
-            },
-            new StatusDeltaKey(Order.Status.COMPLETED, null),
-            (inventoryLevel, change) -> {
-                return inventoryLevel;
-            },
-            new StatusDeltaKey(Order.Status.SUBMITTED, null),
-            (inventoryLevel, change) -> {
-                return inventoryLevel;
-            },
-            new StatusDeltaKey(Order.Status.PROCESSING, null),
-            (inventoryLevel, change) -> {
-                return inventoryLevel;
-            }
-    );
+    private static final Map<StatusDeltaKey, InventoryModifier> callbacks = Map.ofEntries(
+            Map.entry(new StatusDeltaKey(Order.Status.WAIT_LIST, null), (inventoryLevel, change) -> {
+        inventoryLevel.setNeededQuantity(inventoryLevel.getNeededQuantity() + change);
+        return inventoryLevel;
+    }), Map.entry(new StatusDeltaKey(Order.Status.SUBMITTED, Order.Status.WAIT_LIST), (inventoryLevel, change) -> {
+        return inventoryLevel;
+    }), Map.entry(new StatusDeltaKey(Order.Status.PROCESSING, Order.Status.SUBMITTED), (inventoryLevel, change) -> {
+        return inventoryLevel;
+    }), Map.entry(new StatusDeltaKey(Order.Status.COMPLETED, Order.Status.PROCESSING), (inventoryLevel, change) -> {
+        return inventoryLevel;
+    }), Map.entry(new StatusDeltaKey(Order.Status.CANCELLED, Order.Status.WAIT_LIST), (inventoryLevel, change) -> {
+        return inventoryLevel;
+    }), Map.entry(new StatusDeltaKey(Order.Status.CANCELLED, Order.Status.SUBMITTED), (inventoryLevel, change) -> {
+        return inventoryLevel;
+    }), Map.entry(new StatusDeltaKey(Order.Status.CANCELLED, Order.Status.PROCESSING), (inventoryLevel, change) -> {
+        return inventoryLevel;
+    }), Map.entry(new StatusDeltaKey(Order.Status.COMPLETED, null), (inventoryLevel, change) -> {
+        return inventoryLevel;
+    }), Map.entry(new StatusDeltaKey(Order.Status.SUBMITTED, null), (inventoryLevel, change) -> {
+        return inventoryLevel;
+    }), Map.entry(new StatusDeltaKey(Order.Status.PROCESSING, null), (inventoryLevel, change) -> {
+        return inventoryLevel;
+    }), Map.entry(new StatusDeltaKey(Order.Status.WAIT_LIST, Order.Status.WAIT_LIST), (inventoryLevel, change) -> {
+        return inventoryLevel;
+    }), Map.entry(new StatusDeltaKey(Order.Status.PROCESSING, Order.Status.PROCESSING), (inventoryLevel, change) -> {
+        return inventoryLevel;
+    }), Map.entry(new StatusDeltaKey(Order.Status.SUBMITTED, Order.Status.SUBMITTED), (inventoryLevel, change) -> {
+        return inventoryLevel;
+    }));
 
     InventoryModifier get(Order.Status previousStatus, Order.Status currentStatus) {
         return callbacks.get(new StatusDeltaKey(previousStatus, currentStatus));
