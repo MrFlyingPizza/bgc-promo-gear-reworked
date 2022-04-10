0ee8c97457f0b9cd0d3c84630f45631bdec8596f
Martin Curic <MCuric@bgcengineering.ca>
2022-04-10 14:59:54 -0700

    Restored input boxes and location dropdown

--- src/main/java/com/example/bgcpromogearreworked/view/operator/inventory/InventoryViewController.java
+++ src/main/java/com/example/bgcpromogearreworked/view/operator/inventory/InventoryViewController.java
@@ -1,5 +1,7 @@
 package com.example.bgcpromogearreworked.view.operator.inventory;
 
+import com.example.bgcpromogearreworked.api.officelocations.officelocation.OfficeLocationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,8 +12,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class InventoryViewController {
 
 
+    private final OfficeLocationService locationService;
+
+    @Autowired
+    public InventoryViewController(OfficeLocationService locationService) {
+        this.locationService = locationService;
+    }
+
     @GetMapping(path={"", "view"})
     public String showViewInventory(Model model) {
+        model.addAttribute("locations", locationService.getOfficeLocations());
         return "management_panel_pages/inventory/inventory_view";
     }
 
