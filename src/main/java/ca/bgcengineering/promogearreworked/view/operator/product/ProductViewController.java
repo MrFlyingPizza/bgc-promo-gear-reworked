720a3d51eacc83682332a78c6b7a5ffe040ae1db
Puneet Mullhi <pmullhi@sfu.ca>
2022-04-16 15:08:14 -0700

    Options frontend CRUD

--- src/main/java/ca/bgcengineering/promogearreworked/view/operator/product/ProductViewController.java
+++ src/main/java/ca/bgcengineering/promogearreworked/view/operator/product/ProductViewController.java
@@ -1,6 +1,7 @@
 package ca.bgcengineering.promogearreworked.view.operator.product;
 
 import ca.bgcengineering.promogearreworked.api.categories.category.CategoryService;
+import ca.bgcengineering.promogearreworked.api.options.option.OptionService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,9 +13,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class ProductViewController {
 
     private final CategoryService categoryService;
+    private final OptionService optionService;
 
-    public ProductViewController(CategoryService categoryService) {
+    public ProductViewController(CategoryService categoryService, OptionService optionService) {
         this.categoryService = categoryService;
+        this.optionService = optionService;
     }
 
     @GetMapping(path = {"", "view"})
@@ -25,6 +28,7 @@ public class ProductViewController {
     @GetMapping("/create")
     public String showCreateProduct(Model model){
         model.addAttribute("categories", categoryService.getCategories());
+        model.addAttribute("options", optionService.getOptions());
         return "management_panel_pages/product/product_create";
     }
 
