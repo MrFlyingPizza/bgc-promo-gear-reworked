8cdd3428401e3960dcfd0d8e3dff73a4af886ced
Martin Curic <MCuric@bgcengineering.ca>
2022-04-18 12:41:38 -0700

    Added files for store page and generating item tiles from products, categories

---
+++ frontend/src/store/IProductItem.ts
@@ -0,0 +1,25 @@
+export interface IProductItem {
+    id: number,
+    name: string,
+    brand: string,
+    description: string,
+    isWaitlistEnabled: boolean,
+    isBigItem: boolean,
+
+    images: {
+       src: string,
+       position: number,
+    }[]
+
+    variants: {
+        id: number,
+        imageId: number,
+        options: {
+            valueId: number,
+            optionId: number,
+            name: string,
+            value: string
+        }[]
+    }[]
+
+}
