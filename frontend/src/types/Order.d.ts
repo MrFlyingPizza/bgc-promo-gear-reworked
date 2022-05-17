a0364cffbb9fa2285048f8633b7fd935754a917a
Richard Gao <RGao@bgcengineering.ca>
2022-05-17 09:42:28 -0700

    added additional order types

---
+++ frontend/src/types/Order.d.ts
@@ -0,0 +1,33 @@
+import OrderStatus from "types/OrderStatus";
+import OrderType from "types/OrderType";
+import Product from "types/Product";
+import ProductVariant from "types/ProductVariant";
+
+interface OrderOfficeLocation {
+    id: number,
+    name: string
+}
+
+type OrderItemProduct = Pick<Product, "id" | "name" | "category">
+type OrderItemVariant = Omit<ProductVariant, "availability">
+
+interface OrderItem {
+    product: OrderItemProduct,
+    variant: OrderItemVariant,
+    quantity: number
+}
+
+export default interface Order {
+    id: number,
+    submitter: string,
+    fulfiller: string,
+    recipient: string,
+    status: OrderStatus,
+    type: OrderType,
+    submitterComments: string,
+    fulfillerComments: string,
+    officeLocation: OrderOfficeLocation,
+    createdDate: Date,
+    completedDate: Date,
+    items: OrderItem[]
+}
