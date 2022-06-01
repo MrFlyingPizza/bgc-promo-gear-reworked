4d92ce7f91b5ec6bc267709124285372d1a9c682
Richard Gao <RGao@bgcengineering.ca>
2022-06-01 07:47:09 -0700

    implemented big item quantity limit validator

--- src/main/java/ca/bgcengineering/promogearreworked/api/orders/general/dto/validator/GeneralOrderBigItemQuantityLimitCreateValidator.java
+++ src/main/java/ca/bgcengineering/promogearreworked/api/orders/general/dto/validator/GeneralOrderBigItemQuantityLimitCreateValidator.java
@@ -2,21 +2,33 @@ package ca.bgcengineering.promogearreworked.api.orders.general.dto.validator;
 
 import ca.bgcengineering.promogearreworked.api.orders.constraints.BigItemQuantityLimit;
 import ca.bgcengineering.promogearreworked.api.orders.general.dto.GeneralOrderCreate;
+import ca.bgcengineering.promogearreworked.configuration.StoreDefaultsConfig;
+import ca.bgcengineering.promogearreworked.persistence.entities.User;
+import ca.bgcengineering.promogearreworked.persistence.repositories.ProductVariantRepository;
 import ca.bgcengineering.promogearreworked.persistence.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+import java.time.Instant;
 
 public class GeneralOrderBigItemQuantityLimitCreateValidator implements ConstraintValidator<BigItemQuantityLimit, GeneralOrderCreate> {
 
     @Autowired
-    private UserRepository userRepo;
+    private ProductVariantRepository variantRepo;
+
+    @Autowired
+    private StoreDefaultsConfig defaults;
 
     @Override
     public boolean isValid(GeneralOrderCreate orderCreate, ConstraintValidatorContext context) {
         if (orderCreate.getItems() == null || orderCreate.getItems().size() < 1) return true;
-        // TODO: 2022-05-31 implement
-        return false;
+        int totalBigItemQuantity = 0;
+        for (GeneralOrderCreate.NestedOrderItem item : orderCreate.getItems()) {
+            if (variantRepo.getById(item.getVariantId()).getProduct().getIsBigItem()) {
+                totalBigItemQuantity += item.getQuantity();
+            }
+        }
+        return totalBigItemQuantity > defaults.getBigItemOrderQuantityLimit();
     }
 }
