c3ad2e538c992fb4051e08a0031f3903407f8539
Richard Gao <RGao@bgcengineering.ca>
2022-07-18 17:56:23 -0700

    made order info cards on the order page more condense

--- frontend/src/components/orders/OrderCard.tsx
+++ frontend/src/components/orders/OrderCard.tsx
@@ -1,48 +1,69 @@
-import React from "react";
+import React, {useState} from "react";
 import Order from "types/Order";
-import {Accordion, Card} from "react-bootstrap";
-import {Grid} from "@mui/material";
+import {Card} from "react-bootstrap";
+import {Button, Grid, ListItemText, Collapse} from "@mui/material";
 import OrderCardDetails from "components/orders/OrderCardDetails";
 import OrderCardContent from "components/orders/OrderCardContent";
 import OrderStatusLabel from "components/shared/OrderStatusLabel";
+import {format} from "date-fns";
 
 type OrderCardProps = {
     order: Order
 }
 
 const OrderCard = ({order}: OrderCardProps) => {
+
+    const [isOpen, setOpen] = useState(true);
+
+    function toggleOpen() {
+        setOpen(!isOpen)
+    }
+
     return (
         <Card>
             <Card.Header>
-                Order #{order.id}
-                <span className={"ms-2"}>
-                    <OrderStatusLabel status={order.status}/>
-                </span>
-            </Card.Header>
-            <Card.Body>
-                <Grid container spacing={2}>
-                    <Grid item xs={12} md={4}>
-                        <Accordion defaultActiveKey={"0"}>
-                            <Accordion.Item eventKey={"0"}>
-                                <Accordion.Header>Order Details</Accordion.Header>
-                                <Accordion.Body>
-                                    <OrderCardDetails order={order}/>
-                                </Accordion.Body>
-                            </Accordion.Item>
-                        </Accordion>
+                <Grid container spacing={2} justifyContent={"space-evenly"}>
+                    <Grid item>
+                        <ListItemText primary={"Order #"} secondary={order.id}/>
                     </Grid>
-                    <Grid item xs={12} md={8}>
-                        <Accordion defaultActiveKey={"0"}>
-                            <Accordion.Item eventKey={"0"}>
-                                <Accordion.Header>Order Items</Accordion.Header>
-                                <Accordion.Body>
-                                    <OrderCardContent order={order}/>
-                                </Accordion.Body>
-                            </Accordion.Item>
-                        </Accordion>
+                    <Grid item>
+                        <ListItemText primary={"Status"}/>
+                        <div><OrderStatusLabel status={order.status}/></div>
+                    </Grid>
+                    <Grid item>
+                        <ListItemText primary={"Placed On"}
+                                      secondary={order.createdDate ? format(new Date(order.createdDate), "MMM d, yyyy") : "Unknown"}/>
+                    </Grid>
+                    <Grid item>
+                        <ListItemText primary={"Submitter"} secondary={order.submitter ?? "Unknown"}/>
+                    </Grid>
+                    <Grid item>
+                        <ListItemText primary={"Fulfiller"} secondary={order.fulfiller ?? "Unknown"}/>
+                    </Grid>
+                    <Grid item>
+                        <ListItemText primary={"Office"} secondary={order.location?.name ?? "Unknown"}/>
+                    </Grid>
+                    <Grid item>
+                        <ListItemText primary={"Completed On"}
+                                      secondary={order.completedDate ? format(new Date(order.completedDate), "MMM d, yyyy") : "Unknown"}/>
+                    </Grid>
+                    <Grid item xs={"auto"}>
+                        <Button variant={"text"} onClick={toggleOpen}>{isOpen ? "Hide" : "Show"}</Button>
                     </Grid>
                 </Grid>
-            </Card.Body>
+            </Card.Header>
+            <Collapse in={isOpen}>
+                <Card.Body>
+                    <Grid container spacing={4}>
+                        <Grid item xs={12} md={4}>
+                            <OrderCardDetails order={order}/>
+                        </Grid>
+                        <Grid item xs={12} md={8}>
+                            <OrderCardContent order={order}/>
+                        </Grid>
+                    </Grid>
+                </Card.Body>
+            </Collapse>
         </Card>
     );
 };
--- frontend/src/components/orders/OrderCardDetails.tsx
+++ frontend/src/components/orders/OrderCardDetails.tsx
@@ -1,7 +1,6 @@
 import Order from "types/Order";
 import {ListGroup} from "react-bootstrap";
 import React from "react";
-import {format} from "date-fns";
 
 type OrderCardDetailsProps = {
     order: Order
@@ -10,26 +9,6 @@ type OrderCardDetailsProps = {
 const OrderCardDetails = ({order}: OrderCardDetailsProps) => {
     return (
         <ListGroup>
-            <ListGroup.Item>
-                <h6>Submitter</h6>
-                <span>{order.submitter}</span>
-            </ListGroup.Item>
-            <ListGroup.Item>
-                <h6>Fulfiller</h6>
-                <span>{order.fulfiller}</span>
-            </ListGroup.Item>
-            <ListGroup.Item>
-                <h6>Office Location</h6>
-                <span>{order.location?.name}</span>
-            </ListGroup.Item>
-            <ListGroup.Item>
-                <h6>Created Date</h6>
-                <span>{order.createdDate && format(new Date(order.createdDate), "MMM d, yyyy")}</span>
-            </ListGroup.Item>
-            <ListGroup.Item>
-                <h6>Completed Date</h6>
-                <span>{order.completedDate && format(new Date(order.completedDate), "MMM d, yyyy")}</span>
-            </ListGroup.Item>
             <ListGroup.Item>
                 <h6>Comments</h6>
                 <span>{order.submitterComments}</span>
